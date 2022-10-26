68193ef777e2ab9d29a0053a1860e21c1c4e34bf
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2022-10-25 19:12:17 -0700

    first commit

---
+++ variables-practice.js
@@ -0,0 +1,11 @@
+1. //Please return 22
+//you can only add variables
+const a = 10;
+let b = A;
+console.log(a+b);
+
+2. //Please return “This is Coding for the best”
+//use three variables at least
+const FirstPart = "This is ";
+let seCondPart = "not Coding";
+console.log(firstpart+seCondpart);
